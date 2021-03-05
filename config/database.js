cc6dfd4238b40bc29e32a0440659f5f914e82bb6
Adedeji Yusuf <adedejiyusuf26@gmail.com>
2021-03-05 21:14:05 +0100

    Solving The error of Index Page

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // database: "mongodb://localhost:27017/store",
-  database:
-    "mongodb+srv://fidelity_gadgets:morenikeji@26@fidelity-gadgets.pxit4.mongodb.net/fidelity-gadgets?retryWrites=true&w=majority",
+  database: "mongodb://localhost:27017/store",
+  // database:
+  //   "mongodb+srv://fidelity_gadgets:morenikeji@26@fidelity-gadgets.pxit4.mongodb.net/fidelity-gadgets?retryWrites=true&w=majority",
 };
